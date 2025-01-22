739c52cfc0749a1c2e9a570bbb6f9e1c8889e1c0
vik <foo@bar.com>
2025-01-22 12:07:34 +0000

    lightmode UI fixes

--- src/App.jsx
+++ src/App.jsx
@@ -54,29 +54,29 @@ function App() {
           <div className = "text-left">
             <label className="font-bold">Nickname </label>
             <br />
-            <input className = "w-full h-8 rounded dark:bg-gray-800"value={inputNick} onChange={(x) => {setInputNick(x.target.value)}}></input>
+            <input className = "w-full h-8 rounded border border-gray-400 dark:border-0 dark:bg-gray-800"value={inputNick} onChange={(x) => {setInputNick(x.target.value)}}></input>
           </div>
           <div className="text-left">
             <label className="font-bold">Code</label>
             <br />
-            <input className = "w-full h-8 rounded dark:bg-gray-800"value={inputCode} onChange={(x) => {setInputCode(x.target.value)}}></input>
+            <input className = "w-full h-8 rounded border border-gray-400 dark:border-0 dark:bg-gray-800"value={inputCode} onChange={(x) => {setInputCode(x.target.value)}}></input>
           </div>
           
-          <button className="bg-green-200 hover:bg-green-300 rounded py-3 px-3 text-gray-800 text-center font-semibold"onClick={handleAddButton}>ADD</button>
+          <button className="bg-green-400 hover:bg-green-500 dark:bg-green-200 dark:hover:bg-green-300 rounded py-3 px-3 text-gray-800 text-center font-semibold"onClick={handleAddButton}>ADD</button>
         </div>
         <div className="card-list">
           {
             savedCards.map( (x, i) => (
               // seleted item : normal item
-              <center className={(x.expanded) ? "rounded-xl bg-slate-800 grid gap-5 justify-items-center" : "grid gap-5 justify-items-center"}>
+              <center className={(x.expanded) ? "rounded-xl bg-zinc-200 dark:bg-slate-800 grid gap-5 justify-items-center" : "grid gap-5 justify-items-center"}>
                 <h3 className="text-3xl">{x.nick}</h3>
-                <button className="w-24 bg-green-200 hover:bg-green-300 rounded py-3 px-3 text-gray-800 text-center font-semibold" onClick={ () => handleShowButton(i)}>{(x.expanded) ? "HIDE" : "SHOW"}</button>
+                <button className="w-24 bg-green-400 hover:bg-green-500 dark:bg-green-200 dark:hover:bg-green-300 rounded py-3 px-3 text-gray-800 text-center font-semibold" onClick={ () => handleShowButton(i)}>{(x.expanded) ? "HIDE" : "SHOW"}</button>
                 <br />
                 {(x.expanded) ? (
                   <div>
                     <Barcode value={x.code}/>
                     <br />
-                    <button className = "bg-red-200 hover:bg-red-300 rounded py-3 px-3 text-gray-800 text-center font-semibold" onClick={ () => handleDeleteButton(i)}>DELETE 🔥</button>
+                    <button className = "bg-red-300 hover:bg-red-400 dark:bg-red-200 dark:hover:bg-red-300 rounded py-3 px-3 text-gray-800 text-center font-semibold" onClick={ () => handleDeleteButton(i)}>DELETE 🔥</button>
                   </div>
                 ) : (<></>)}
               </center>
